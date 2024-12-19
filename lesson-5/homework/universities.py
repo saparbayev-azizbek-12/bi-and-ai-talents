9ba97fffb6c874e660029efc3d92cdf67b98d874
saparbayev-azizbek-01 <saparbayev.azizbek12@gmail.com>
2024-12-19 21:27:19 +0500

    lesson-5 completed

--- lesson-5/homework/universities.py
+++ lesson-5/homework/universities.py
@@ -1,10 +1,17 @@
-def enrollment_stats(a: list) -> list:
-    student, tuition = 0, 0
+def enrollment_stats(a: list, index: int) -> int:
+    summa = 0
     for i in a:
-        for j in i:
-            student += j[1]
-            tuition += j[2]
-    return [student, tuition]
+        summa += i[index]
+    return summa
+    
+
+def mean(a: list, index: int) -> int:
+    return f"{(sum([i[index] for i in a])/len(a)):.2f}"
+
+def median(a: list, index: int) -> int:
+    a = sorted(a, key=lambda x: x[index])
+    return a[len(a)//2][index]
+
 
 universities = [
     ['California Institute of Technology', 2175, 37704],
@@ -16,3 +23,12 @@ universities = [
     ['Yale', 11701, 40500]
 ]
 
+print('*'*50)
+print('Total student:',enrollment_stats(universities, 1))
+print('Total tuition: $',enrollment_stats(universities, 2))
+print()
+print('Student mean:',mean(universities, 1))
+print('Student median:',median(universities, 1))
+print()
+print('Tuition mean: $',mean(universities, 2))
+print('Tuition median: $',median(universities, 2))
