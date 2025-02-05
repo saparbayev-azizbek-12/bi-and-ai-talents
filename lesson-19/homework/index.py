4fc9c24f6b8e8e45449fb9ff83c838385f1032cc
saparbayev-azizbek-01 <saparbayev.azizbek12@gmail.com>
2025-02-05 11:18:11 +0500

    lesson-19 completed

---
+++ lesson-19/homework/index.py
@@ -0,0 +1,12 @@
+matn = input()
+
+unlilar = "AOIUEaoiue"
+
+javob = ""
+
+for i in matn:
+    if i in unlilar:
+        javob += i
+   
+
+print(javob)
--- task_homework/first_page_pdf.py
+++ task_homework/first_page_pdf.py
@@ -1,6 +1,6 @@
 import os
 import re
-import fitz
+import fitz #PyMuPDF
 
 def get_image_of_pdf(path):
     with fitz.open(path) as pdf_document:
@@ -8,7 +8,10 @@ def get_image_of_pdf(path):
         directory = r"C:\Users\ADMIN\Desktop\bi-and-ai-talents\task_homework\media"
         
         exiting_numbers = [int(re.findall(r'\d+', f)[0]) for f in os.listdir(directory) if f.startswith("first_page_") and f.endswith(".png")]
-        number = max(exiting_numbers) + 1
+        if exiting_numbers:
+            number = max(exiting_numbers) + 1
+        else:
+            number = 1
 
         first_page = pdf_document[0]
         pix = first_page.get_pixmap()
